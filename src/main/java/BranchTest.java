fef60ed6011d2fbe2debc26fdb7d7fe3aa71fc25
yuebuqun <18579099920@163.com>
2019-04-27 20:39:58 +0800

    分支测试new Branch

--- src/main/java/BranchTest.java
+++ src/main/java/BranchTest.java
@@ -6,5 +6,6 @@ public class BranchTest {
     public static void main(String[] args){
         System.out.println("分支测试");
         System.out.println("分支测试");
+        System.out.println("新的branch");
     }
 }
